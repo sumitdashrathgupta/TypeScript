54115d87b989d04a2436aec749c97276050114e8
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2025-03-29 09:55:19 +0530

    update

---
+++ Part 2/Day 4/Type_Guards/index.js
@@ -0,0 +1,13 @@
+function abc(a) {
+    if (typeof a === "number") {
+        return "number";
+    }
+    else if (typeof a === "string") {
+        return 'string';
+    }
+    else {
+        throw new Error("THis is as error");
+    }
+}
+abc(12);
+abc("string");
